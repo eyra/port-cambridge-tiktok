74baf87285bf75e7352766251833612a2aaf90fa
Kasper Welbers <kasperwelbers@gmail.com>
2023-11-17 13:00:35 +0100

    husky, you ok?

--- src/framework/visualisation/react/ui/prompts/consent_form.tsx
+++ src/framework/visualisation/react/ui/prompts/consent_form.tsx
@@ -52,24 +52,24 @@ export const ConsentForm = (props: Props): JSX.Element => {
     })
   }, [])
 
-  function rowCell (dataFrame: any, column: string, row: number): PropsUITableCell {
+  function rowCell(dataFrame: any, column: string, row: number): PropsUITableCell {
     const text = String(dataFrame[column][`${row}`])
     return { __type__: 'PropsUITableCell', text: text }
   }
 
-  function headCell (dataFrame: any, column: string): PropsUITableCell {
+  function headCell(dataFrame: any, column: string): PropsUITableCell {
     return { __type__: 'PropsUITableCell', text: column }
   }
 
-  function columnNames (dataFrame: any): string[] {
+  function columnNames(dataFrame: any): string[] {
     return Object.keys(dataFrame)
   }
 
-  function columnCount (dataFrame: any): number {
+  function columnCount(dataFrame: any): number {
     return columnNames(dataFrame).length
   }
 
-  function rowCount (dataFrame: any): number {
+  function rowCount(dataFrame: any): number {
     if (columnCount(dataFrame) === 0) {
       return 0
     } else {
@@ -78,9 +78,10 @@ export const ConsentForm = (props: Props): JSX.Element => {
     }
   }
 
-  function rows (data: any): PropsUITableRow[] {
+  function rows(data: any): PropsUITableRow[] {
     const result: PropsUITableRow[] = []
-    for (let row = 0; row <= rowCount(data); row++) {
+    const n = rowCount(data)
+    for (let row = 0; row <= n; row++) {
       const id = `${row}`
       const cells = columnNames(data).map((column: string) => rowCell(data, column, row))
       result.push({ __type__: 'PropsUITableRow', id, cells })
@@ -88,13 +89,13 @@ export const ConsentForm = (props: Props): JSX.Element => {
     return result
   }
 
-  function parseTables (
+  function parseTables(
     tablesData: PropsUIPromptConsentFormTable[]
   ): Array<PropsUITable & TableContext> {
     return tablesData.map((table) => parseTable(table))
   }
 
-  function parseTable (tableData: PropsUIPromptConsentFormTable): PropsUITable & TableContext {
+  function parseTable(tableData: PropsUIPromptConsentFormTable): PropsUITable & TableContext {
     const id = tableData.id
     const title = Translator.translate(tableData.title, props.locale)
     const deletedRowCount = 0
@@ -117,33 +118,33 @@ export const ConsentForm = (props: Props): JSX.Element => {
     }
   }
 
-  function handleDonate (): void {
+  function handleDonate(): void {
     const value = serializeConsentData()
     resolve?.({ __type__: 'PayloadJSON', value })
   }
 
-  function handleCancel (): void {
+  function handleCancel(): void {
     resolve?.({ __type__: 'PayloadFalse', value: false })
   }
 
-  function serializeConsentData (): string {
+  function serializeConsentData(): string {
     const array = serializeTables().concat(serializeMetaData())
     return JSON.stringify(array)
   }
 
-  function serializeMetaData (): any[] {
+  function serializeMetaData(): any[] {
     return serializeMetaTables().concat(serializeDeletedMetaData())
   }
 
-  function serializeTables (): any[] {
+  function serializeTables(): any[] {
     return tables.map((table) => serializeTable(table))
   }
 
-  function serializeMetaTables (): any[] {
+  function serializeMetaTables(): any[] {
     return metaTables.map((table) => serializeTable(table))
   }
 
-  function serializeDeletedMetaData (): any {
+  function serializeDeletedMetaData(): any {
     const rawData = tables
       .filter(({ deletedRowCount }) => deletedRowCount > 0)
       .map(({ id, deletedRowCount }) => `User deleted ${deletedRowCount} rows from table: ${id}`)
@@ -152,12 +153,12 @@ export const ConsentForm = (props: Props): JSX.Element => {
     return { user_omissions: data }
   }
 
-  function serializeTable ({ id, head, body: { rows } }: PropsUITable): any {
+  function serializeTable({ id, head, body: { rows } }: PropsUITable): any {
     const data = rows.map((row) => serializeRow(row, head))
     return { [id]: data }
   }
 
-  function serializeRow (row: PropsUITableRow, head: PropsUITableHead): any {
+  function serializeRow(row: PropsUITableRow, head: PropsUITableHead): any {
     assert(
       row.cells.length === head.cells.length,
       `Number of cells in row (${row.cells.length}) should be equals to number of cells in head (${head.cells.length})`
@@ -169,11 +170,11 @@ export const ConsentForm = (props: Props): JSX.Element => {
 
   return (
     <>
-      <div className='max-w-3xl'>
+      <div className="max-w-3xl">
         <BodyLarge text={description} />
       </div>
-      <div className='flex flex-col gap-16 w-full'>
-        <div className='grid gap-8 max-w-full'>
+      <div className="flex flex-col gap-16 w-full">
+        <div className="grid gap-8 max-w-full">
           {tables.map((table) => {
             return (
               <TableContainer
@@ -187,15 +188,15 @@ export const ConsentForm = (props: Props): JSX.Element => {
           })}
         </div>
         <div>
-          <BodyLarge margin='' text={donateQuestion} />
+          <BodyLarge margin="" text={donateQuestion} />
 
-          <div className='flex flex-row gap-4 mt-4 mb-4'>
+          <div className="flex flex-row gap-4 mt-4 mb-4">
             <PrimaryButton
               label={donateButton}
               onClick={handleDonate}
-              color='bg-success text-white'
+              color="bg-success text-white"
             />
-            <LabelButton label={cancelButton} onClick={handleCancel} color='text-grey1' />
+            <LabelButton label={cancelButton} onClick={handleCancel} color="text-grey1" />
           </div>
         </div>
       </div>
@@ -210,7 +211,7 @@ interface Copy {
   cancelButton: string
 }
 
-function prepareCopy ({ locale }: Props): Copy {
+function prepareCopy({ locale }: Props): Copy {
   return {
     description: Translator.translate(description, locale),
     donateQuestion: Translator.translate(donateQuestionLabel, locale),
